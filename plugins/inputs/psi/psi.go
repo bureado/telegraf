0983066fb73284fd9819ce9783b8bb2b8d2074f4
Jose Miguel Parrella <jparrel@microsoft.com>
2019-11-07 04:32:25 +0000

    Adding remaining values for the proof of concept

--- plugins/inputs/psi/psi.go
+++ plugins/inputs/psi/psi.go
@@ -21,17 +21,38 @@ func (*PSIStats) SampleConfig() string {
 
 func (s *PSIStats) Gather(acc telegraf.Accumulator) error {
 
+	// TODO: error handling, constant for /proc
 	fs, _ := procfs.NewFS("/proc")
 
 	fields := make(map[string]interface{})
 
+	// TODO: iterator of some sort
 	vals, _ := fs.PSIStatsForResource("io")
 
-        fields["Avg10"] = vals.Some.Avg10
-        fields["Avg60"] = vals.Some.Avg60
-        fields["Total"] = vals.Some.Total
+        fields["IOSome10"] = vals.Some.Avg10
+        fields["IOSome60"] = vals.Some.Avg60
+        fields["IOFull10"] = vals.Full.Avg10
+        fields["IOFull60"] = vals.Full.Avg60
+        fields["IOSomeTotal"] = vals.Some.Total
+        fields["IOFullTotal"] = vals.Full.Total
 
-	acc.AddFields("psiIO", fields, nil)
+	vals, _ = fs.PSIStatsForResource("cpu")
+
+        fields["CPUSome10"] = vals.Some.Avg10
+        fields["CPUSome60"] = vals.Some.Avg60
+        fields["CPUSomeTotal"] = vals.Some.Total
+
+	vals, _ = fs.PSIStatsForResource("memory")
+
+        fields["MEMFull10"] = vals.Full.Avg10
+        fields["MEMFull60"] = vals.Full.Avg60
+        fields["MEMSome10"] = vals.Some.Avg10
+        fields["MEMSome60"] = vals.Some.Avg60
+        fields["MEMSomeTotal"] = vals.Some.Total
+        fields["MEMFullTotal"] = vals.Full.Total
+
+	// TODO: alternatively, have a full/some field or 10/60/total fields
+	acc.AddFields("psi", fields, nil)
 
 	return nil
 }
